1109fc7bf549ab1df09aa9ace2639f09e13d5fd0
Sapphira Tempest <freeze-dolphin@outlook.com>
2024-02-04 23:06:11 +0800

    add notification after generation is done

--- monitor.sh
+++ monitor.sh
@@ -6,12 +6,13 @@ EVENTS="modify"
 
 switch=1
 
-inotifywait -m -r -e "$EVENTS" "${TARGET}.py" | while read -r _ action _; do
+inotifywait -m -e "$EVENTS" "${TARGET}.py" | while read -r _ action _; do
     if [ $switch = 1 ]; then
         echo "[I] Detected $action on target $TARGET - $(date +"%T")"
         if [[ -e "./args/$TARGET" ]]; then
             echo "[I] Generating from template args..."
             python "${TARGET}.py" < "./args/${TARGET}" > "result/${TARGET}.aff"
+            echo "[I] Generated"
         else
             echo "[E] Argument template not exist" >&2
         fi
