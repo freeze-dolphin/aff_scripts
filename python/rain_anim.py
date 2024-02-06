3b2ea7af8af6eb323bc860ba75aa4a2eed3ea6a3
Sapphira Tempest <freeze-dolphin@outlook.com>
2024-02-06 18:11:16 +0800

    feat: rain animation

--- python/rain_anim.py
+++ python/rain_anim.py
@@ -1,5 +1,7 @@
+import math
 import random as rnd
 
+import utils
 from easing import *
 from utils import gen_trace, calc_pos
 
@@ -7,9 +9,14 @@ from utils import gen_trace, calc_pos
 # 基本参数
 # ------------------------------------------------------
 
-NOTE_OFFSET = 150000
-BPM = 175
-FRAME_COUNT = 180
+# 整首歌的长度
+NOTE_OFFSET = 223709
+
+# 控制整体速度
+BPM = 130
+
+# 帧数
+FRAME_COUNT = 120
 
 
 # ------------------------------------------------------
@@ -40,9 +47,9 @@ def gen_frame(
     hide_timing = int(hide_timing)
     _result = [
                   gen_timing(0, BPM),
-                  gen_timing(show_timing - 1, -BPM * NOTE_OFFSET),
+                  gen_timing(show_timing - 1, -BPM * NOTE_OFFSET - extra_note_offset),
                   gen_timing(show_timing, 0),
-                  gen_timing(hide_timing - 1, -BPM * NOTE_OFFSET),
+                  gen_timing(hide_timing - 1, -BPM * NOTE_OFFSET - extra_note_offset),
                   gen_timing(hide_timing, BPM),
               ] + gen_arcs(
         hide_timing + NOTE_OFFSET + extra_note_offset,
@@ -70,7 +77,7 @@ def gen_anim(
     position_diff = calc_pos(
         _end_position, _start_position, "-"
     )
-    real_frame_count = int(FRAME_COUNT * (_duration / 1000.0))  # 计算出需要处理多少帧后
+    real_frame_count = int(FRAME_COUNT * (_duration / 1000.0))
     _result = []
 
     if rnd.random() < _small_droplet_ratio:
@@ -82,6 +89,15 @@ def gen_anim(
         progress = i / (
                 real_frame_count - 1.0
         )
+
+        nextprogress = (i + 1) / (real_frame_count - 1.0)
+        _timing = _timing + _duration * progress
+        _nextTiming = _timing + _duration * nextprogress
+
+        if abs(_nextTiming - 205936) < 30:
+            utils.eprint(_nextTiming)
+            continue
+
         _result.append("timinggroup(noinput){")
         position = calc_pos(
             _start_position,
@@ -92,10 +108,6 @@ def gen_anim(
             ),
             "+",
         )
-
-        nextprogress = (i + 1) / (real_frame_count - 1.0)
-        _timing = _timing + _duration * progress
-        _nextTiming = _timing + _duration * nextprogress
         _result += gen_frame(
             _timing,
             _nextTiming,
@@ -111,6 +123,7 @@ def gen_anim(
 
 
 def gen_rain(_timings,
+             _timing_step,
              _side_overlapping,
              _mid_overlapping,
              _x_left_rng,
@@ -124,7 +137,8 @@ def gen_rain(_timings,
              _small_droplet_ratio,
              _small_droplet_length_rng,
              _large_droplet_length_rng,
-             _swap_start_and_end_position,
+             _ground_y=0.0,
+             _swap_start_and_end_position=False,
              ):
     """
     _timings: 下雨时间, 即动画生成的时间段
@@ -144,7 +158,7 @@ def gen_rain(_timings,
     _swap_start_and_end_position: 反转雨滴起落
     """
 
-    timings = range(*_timings, 20)
+    timings = range(*_timings, _timing_step)
 
     _x_offset = math.tan(_radians) * (sum(_y_rng) / 2)
     _x_right_rng = tuple(map(lambda _x: _x + _x_offset, _x_right_rng))
@@ -154,25 +168,32 @@ def gen_rain(_timings,
     _result = []
 
     for timing in timings:
-        choice = rnd.randint(0, 2)
         overlapping = _side_overlapping
+        if _x_mid_rng[0] != _x_mid_rng[1]:
+            choice = rnd.randint(0, 2)
 
-        if choice == 0:
-            x = rnd.uniform(*_x_right_rng)
-        elif choice == 1:
-            x = rnd.uniform(*_x_left_rng)
+            if choice == 0:
+                x = rnd.uniform(*_x_right_rng)
+            elif choice == 1:
+                x = rnd.uniform(*_x_left_rng)
+            else:
+                x = rnd.uniform(*_x_mid_rng)
+                overlapping = _mid_overlapping
         else:
-            x = rnd.uniform(*_x_mid_rng)
-            overlapping = _mid_overlapping
+            choice = rnd.randint(0, 1)
+            if choice == 0:
+                x = rnd.uniform(*_x_right_rng)
+            else:
+                x = rnd.uniform(*_x_left_rng)
 
         y = rnd.uniform(*_y_rng)
 
         if _swap_start_and_end_position:
-            _start_pos = (x - math.tan(_radians) * y, 0)
+            _start_pos = (x - math.tan(_radians) * y, _ground_y)
             _end_pos = (x, y)
         else:
             _start_pos = (x, y)
-            _end_pos = (x - math.tan(_radians) * y, 0)
+            _end_pos = (x - math.tan(_radians) * y, _ground_y)
 
         _result += gen_anim(
             timing,
@@ -195,43 +216,197 @@ def gen_rain(_timings,
 
 result = []
 
+"""
+75782-87320：黑线上升 [✔]
+87320-90090：上升的黑线慢慢停止 [✔]
+
+92397-95166：停止的黑线转为下雨 [✔]
+95166-96090：停雨 [✔]
+
+96090-98859：下雨（少量） [✔]
+98859-103474：停雨 [✔]
+
+103474-105320：下雨（少量） [✔]
+105320-107166：停雨 [✔]
+
+107166-108090：雨只下到一半停止下，没接触地面 [✔]
+108090-109936：下雨（少量） [✔]
+109936-110859：停雨 [✔]
+
+110859-116859：下雨（微少量） [✔]
+116859-118243：停雨 [✔]
+118243-121936：下雨 [✔]
+
+176859-205936: 一直下雨 []
+"""
+
 result += gen_rain(
-    (500, 3000),  # 注意 3000 表示动画生成的结束时间, 而不是动画结束时间, 动画生成结束时间 + 动画过程时间 才是动画结束时间
+    (75782, 89800),
+    80,
+    2,
+    1,
+    (-0.3, -1.0),
+    (1.3, 2.0),
+    (0, 0),
+    (2.2, 2.2),
+    (0, 2800),
+    (173, 173),
+    math.radians(0),
+    ease_out_quad,
+    1.0,
+    (0.2, 0.35),
+    (0, 0),
+    _swap_start_and_end_position=True,
+)
+
+result += gen_rain(
+    (92397 - 90, 95116 - 90),
+    20,
     2,
     1,
     (-0.5, -1.0),
     (1.5, 2.0),
     (-0.5, 1.5),
-    (3.0, 3.0),
-    (0, 1400),
-    (100, 200),
-    math.radians(0),
+    (2.2, 2.2),
+    (0, 2800),
+    (50, 50),
+    math.radians(9),
     ease_linear,
-    1.0,
-    (0.2, 0.35),
-    (0, 0),
-    True
+    0.4,
+    (0.1, 0.2),
+    (0.5, 0.8),
 )
 
 result += gen_rain(
-    (5000, 9000),
-    2,
+    (96090 - 90, 98759 - 90),
+    60,
     1,
-    (-0.5, -0.8),
-    (1.5, 1.8),
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
     (-0.5, 1.5),
-    (2.0, 2.0),
-    (0, 1400),
-    (40, 80),
-    math.radians(9),
+    (2.2, 2.2),
+    (0, 2800),
+    (50, 75),
+    math.radians(4),
     ease_linear,
     0.5,
-    (0.2, 0.35),
-    (0.6, 0.8),
-    False
+    (0.1, 0.2),
+    (0.4, 0.6),
+)
+
+result += gen_rain(
+    (103474 - 90, 105220 - 90),
+    60,
+    1,
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
+    (-0.5, 1.5),
+    (2.2, 2.2),
+    (0, 2800),
+    (50, 75),
+    math.radians(2),
+    ease_linear,
+    0.6,
+    (0.1, 0.2),
+    (0.4, 0.6),
+)
+
+result += gen_rain(
+    (107166 - 90, 107940 - 150 - 90),
+    30,
+    2,
+    1,
+    (0, -0.8),
+    (1, 1.8),
+    (0, 1),
+    (2.2, 2.2),
+    (0, 2000),
+    (100, 120),
+    math.radians(4),
+    ease_out_cubic,
+    0.6,
+    (0.1, 0.2),
+    (0.3, 0.5),
+    _ground_y=0.75
+)
+
+result += gen_rain(
+    (108090 - 90, 109886 - 90),
+    40,
+    1,
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
+    (-0.5, 1.5),
+    (2.2, 2.2),
+    (0, 2800),
+    (50, 50),
+    math.radians(4),
+    ease_linear,
+    0.4,
+    (0.1, 0.2),
+    (0.4, 0.6),
+)
+
+result += gen_rain(
+    (110859 - 90, 116779 - 90),
+    90,
+    1,
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
+    (-0.5, 1.5),
+    (2.2, 2.2),
+    (0, 2800),
+    (60, 80),
+    math.radians(2),
+    ease_linear,
+    0.4,
+    (0.1, 0.2),
+    (0.4, 0.6),
+)
+
+result += gen_rain(
+    (118243 - 90, 121886 - 90),
+    20,
+    2,
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
+    (-0.5, 1.5),
+    (2.2, 2.2),
+    (0, 2800),
+    (50, 50),
+    math.radians(9),
+    ease_linear,
+    0.6,
+    (0.1, 0.2),
+    (0.5, 0.8),
+)
+
+result += gen_rain(
+    (176859, 205936),
+    70,
+    2,
+    1,
+    (-0.5, -1.5),
+    (1.5, 2.5),
+    (-0.5, 1.5),
+    (2.2, 2.2),
+    (0, 1400),
+    (50, 50),
+    math.radians(43.09),
+    ease_linear,
+    0.6,
+    (0.1, 0.2),
+    (0.5, 0.8),
+    _swap_start_and_end_position=True
 )
 
 print("AudioOffset:0\n-")
 print(gen_timing(0, BPM).strip())
+# print("scenecontrol(0,enwidencamera,1000.00,1);")
 
 print("\n".join(result))
