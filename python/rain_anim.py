1517ead7e243ee5dffaf384832645068943a4ceb
Sapphira Tempest <freeze-dolphin@outlook.com>
2024-02-06 13:51:17 +0800

    feat: support for multi rain generations

--- python/rain_anim.py
+++ python/rain_anim.py
@@ -3,13 +3,6 @@ import random as rnd
 from easing import *
 from utils import gen_trace, calc_pos
 
-_SQRT = math.sqrt
-_SIN = math.sin
-_COS = math.cos
-
-_PI = 3.14159265
-_HALF_PI = 1.57079633
-
 # ------------------------------------------------------
 # 基本参数
 # ------------------------------------------------------
@@ -17,55 +10,6 @@ _HALF_PI = 1.57079633
 NOTE_OFFSET = 150000
 BPM = 175
 FRAME_COUNT = 180
-
-# 下雨的时间段
-TIMING = (1000, 3000)
-
-# 下雨的 y 坐标随机范围 (定义天空的 y 坐标)
-Y_RNG = (2.0, 2.0)
-
-# 下雨的前后随机范围 (0 是 判定平面, 1500 是判定平面往前推 1500ms)
-Z_RNG = (0, 1500)
-
-# 雨滴持续时间随机范围, 20ms 到 50ms 之间
-DURATION_RNG = (20, 50)
-
-# 下「小型雨滴」的概率
-SMALL_DROPLET_RATIO = 0.45
-
-# 「小型雨滴」的长度随机范围
-SMALL_DROPLET_LENGTH_RNG = (0.1, 0.3)
-
-# 「大型雨滴」的长度随机范围
-LARGE_DROPLET_LENGTH_RNG = (0.5, 0.8)
-
-# 每组雨滴的数量范围, 用于调整雨点密度
-GROUP_COUNT_RNG = (1, 3)
-
-# 两边的雨滴厚度 (叠层)
-SIDE_OVERLAPPING = 3
-
-# 中间的雨滴厚度 (叠层)
-MID_OVERLAPPING = 1
-
-# 雨点倾斜的弧度, 用 math.radians() 将角度转换成弧度制
-# 正角是从右往左下
-# 负角反之
-RADIANS = math.radians(9)
-
-# X 坐标的偏移量, 取平均
-X_OFFSET = math.tan(RADIANS) * (sum(Y_RNG) / 2)
-
-# 左侧下雨的 x 坐标范围
-# calc_pos( (x, y), X_OFFSET, "+") 表示在 (x, y) 的基础上向右偏移 X_OFFSET
-# 因为雨是倾斜的
-X_LEFT_RNG = calc_pos((-0.5, -0.8), X_OFFSET, "+")
-
-# 右侧下雨的 x 坐标范围
-X_RIGHT_RNG = calc_pos((1.5, 1.8), X_OFFSET, "+")
-
-# 中间下雨的 x 坐标范围
-X_MID_RNG = calc_pos((-0.5, 1.5), X_OFFSET, "+")
 
 
 # ------------------------------------------------------
@@ -76,21 +20,21 @@ def gen_timing(t, b):
     return f"  timing({t:.0f},{b:.2f},999.00);"
 
 
-def gen_arcs(t, _x, _y, _droplet_length, _overlapping):
+def gen_arcs(t, _x, _y, _droplet_length, _overlapping, _radians):
     arcs = []
 
-    delta_y = _droplet_length * math.cos(RADIANS)
+    delta_y = _droplet_length * math.cos(_radians)
 
     for _ in range(_overlapping):
         arcs.append(
-            gen_trace(t, (_x - math.tan(RADIANS) * delta_y, _y - delta_y), (_x, _y))
+            gen_trace(t, (_x - math.tan(_radians) * delta_y, _y - delta_y), (_x, _y))
         )
 
     return arcs
 
 
 def gen_frame(
-        show_timing, hide_timing, position, extra_note_offset, _droplet_length, _overlapping
+        show_timing, hide_timing, position, extra_note_offset, _droplet_length, _overlapping, _radians
 ):
     show_timing = int(show_timing)
     hide_timing = int(hide_timing)
@@ -104,7 +48,8 @@ def gen_frame(
         hide_timing + NOTE_OFFSET + extra_note_offset,
         *position,
         _droplet_length,
-        _overlapping
+        _overlapping,
+        _radians
     )
     return _result
 
@@ -117,6 +62,10 @@ def gen_anim(
         _easing_function,
         _extra_note_offset,
         _overlapping,
+        _radians,
+        _small_droplet_ratio,
+        _small_droplet_length_rng,
+        _large_droplet_length_rng
 ):
     position_diff = calc_pos(
         _end_position, _start_position, "-"
@@ -124,10 +73,10 @@ def gen_anim(
     real_frame_count = int(FRAME_COUNT * (_duration / 1000.0))  # 计算出需要处理多少帧后
     _result = []
 
-    if rnd.random() < SMALL_DROPLET_RATIO:
-        droplet_length = rnd.uniform(*SMALL_DROPLET_LENGTH_RNG)
+    if rnd.random() < _small_droplet_ratio:
+        droplet_length = rnd.uniform(*_small_droplet_length_rng)
     else:
-        droplet_length = rnd.uniform(*LARGE_DROPLET_LENGTH_RNG)
+        droplet_length = rnd.uniform(*_large_droplet_length_rng)
 
     for i in range(real_frame_count):
         progress = i / (
@@ -153,43 +102,116 @@ def gen_anim(
             position,
             _extra_note_offset,
             droplet_length,
-            _overlapping
+            _overlapping,
+            _radians
         )
 
         _result.append("};")
     return _result
 
 
-timings = range(*TIMING, 20)
+def gen_rain(_timings,  # 下雨时间, 即动画生成的时间段
+             _side_overlapping,  # 两边雨滴层数
+             _mid_overlapping,  # 中间雨滴层数
+             _x_left_rng,  # 左边下雨的 x 范围
+             _x_right_rng,  # 右边下雨的 x 范围
+             _x_mid_rng,  # 中间下雨的 x 范围
+             _y_rng,  # 下雨的 y 范围 (定义天空)
+             _z_rng,  # 下雨的 前后 z 范围
+             _duration_rng,  # 每个雨滴的持续时间范围
+             _radians,  # 雨滴倾斜角度的弧度制
+             _easing_function,  # 缓动函数
+             _small_droplet_ratio,  # 下小雨滴的概率
+             _small_droplet_length_rng,  # 小雨滴的长度范围
+             _large_droplet_length_rng,  # 大雨滴的长度范围
+             _swap_start_and_end_position,  # 反转雨滴起落
+             ):
+    timings = range(*_timings, 20)
+
+    _x_offset = math.tan(_radians) * (sum(_y_rng) / 2)
+    _x_right_rng = tuple(map(lambda _x: _x + _x_offset, _x_right_rng))
+    _x_left_rng = tuple(map(lambda _x: _x + _x_offset, _x_left_rng))
+    _x_mid_rng = tuple(map(lambda _x: _x + _x_offset, _x_mid_rng))
+
+    _result = []
+
+    for timing in timings:
+        choice = rnd.randint(0, 2)
+        overlapping = _side_overlapping
+
+        if choice == 0:
+            x = rnd.uniform(*_x_right_rng)
+        elif choice == 1:
+            x = rnd.uniform(*_x_left_rng)
+        else:
+            x = rnd.uniform(*_x_mid_rng)
+            overlapping = _mid_overlapping
+
+        y = rnd.uniform(*_y_rng)
+
+        if _swap_start_and_end_position:
+            _start_pos = (x - math.tan(_radians) * y, 0)
+            _end_pos = (x, y)
+        else:
+            _start_pos = (x, y)
+            _end_pos = (x - math.tan(_radians) * y, 0)
+
+        _result += gen_anim(
+            timing,
+            rnd.randint(*_duration_rng),
+            _start_pos,
+            _end_pos,
+            ease_linear,
+            rnd.randint(*_z_rng),
+            overlapping,
+            _radians,
+            _small_droplet_ratio,
+            _small_droplet_length_rng,
+            _large_droplet_length_rng)
+    return _result
+
+
+# ------------------------------------------------------
+# 降雨参数
+# ------------------------------------------------------
 
 result = []
 
-extra_offset = 0
+result += gen_rain(
+    (500, 3000),  # 注意 3000 表示动画生成的结束时间, 而不是动画结束时间, 动画生成结束时间 + 动画过程时间 才是动画结束时间
+    2,
+    1,
+    (-0.5, -1.0),
+    (1.5, 2.0),
+    (-0.5, 1.5),
+    (3.0, 3.0),
+    (0, 1400),
+    (100, 200),
+    math.radians(0),
+    ease_linear,
+    1.0,
+    (0.2, 0.35),
+    (0, 0),
+    True
+)
 
-for timing in timings:
-    choice = rnd.randint(0, 2)
-    overlapping = SIDE_OVERLAPPING
-
-    if choice == 0:
-        x = rnd.uniform(*X_RIGHT_RNG)
-    elif choice == 1:
-        x = rnd.uniform(*X_LEFT_RNG)
-    else:
-        x = rnd.uniform(*X_MID_RNG)
-        overlapping = MID_OVERLAPPING
-
-    y = rnd.uniform(*Y_RNG)
-
-    result += gen_anim(
-        timing,
-        rnd.randint(*DURATION_RNG),
-        (x, y),
-        (x - math.tan(RADIANS) * y, 0),
-        ease_linear,
-        rnd.randint(*Z_RNG),
-        overlapping)
-
-PER_OFFSET = 5
+result += gen_rain(
+    (5000, 9000),
+    2,
+    1,
+    (-0.5, -0.8),
+    (1.5, 1.8),
+    (-0.5, 1.5),
+    (2.0, 2.0),
+    (0, 1400),
+    (40, 80),
+    math.radians(9),
+    ease_linear,
+    0.5,
+    (0.2, 0.35),
+    (0.6, 0.8),
+    False
+)
 
 print("AudioOffset:0\n-")
 print(gen_timing(0, BPM).strip())
